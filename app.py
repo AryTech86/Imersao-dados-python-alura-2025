ab6f2c95495ffea087988782079f311efd86d94e
Ariadne Moura <142263596+AryTech86@users.noreply.github.com>
2025-08-12 10:18:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+!pip install plotly
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
@@ -137,4 +138,5 @@ st.subheader("Dados Detalhados")
 st.dataframe(df_filtrado)
 
      
+
 
